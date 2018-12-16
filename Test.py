109cb4004cf88ce77e040760f12ecc2a06cc719a
Ethan Jackson <Ethanjackson12345@gmail.com>
2018-12-16 00:03:07 -0600

    Update Test.py

--- Test.py
+++ Test.py
@@ -11,8 +11,5 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.add_event_detect(3,GPIO.RISING,callback=button_callback1) # Setup event on pin 10 rising edge
 
-GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.add_event_detect(12,GPIO.RISING,callback=button_callback2) # Setup event on pin 10 rising edge
-
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 GPIO.cleanup() # Clean up
